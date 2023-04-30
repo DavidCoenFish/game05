322cfe7112b93df53d097728651277a2906da0a0
DavidCoenFish <fish_dsc@yahoo.co.uk>
2023-04-30 17:12:00 +1000

    2023-04-30T17:11:58 DESKTOP-LD41L9F

--- prototype/gpu_character_contoller_00/source/ApplicationTriangle.cpp
+++ prototype/gpu_character_contoller_00/source/ApplicationTriangle.cpp
@@ -5,6 +5,47 @@
 #include "DrawSystemD12/Shader/Shader.h"
 #include "DrawSystemD12/Geometry/GeometryGeneric.h"
 #include "DrawSystemD12/DrawSystemFrame.h"
+#include "DrawSystemD12/CustomCommandList.h"
+
+namespace
+{
+	// based on code Copyright (c) Microsoft Corporation. All rights reserved. ReadData.h
+	const std::shared_ptr<std::vector< uint8_t >> SyncReadFile(const std::filesystem::path& absolutePath)
+	{
+	   std::ifstream inFile(absolutePath, std::ios::in | std::ios::binary | std::ios::ate);
+	   auto pBlob = std::make_shared< std::vector< uint8_t > >();
+   
+	   if ((!inFile) || (inFile.fail()))
+	   {
+		  throw std::exception("SyncReadFile");
+		  //return pBlob;
+	   }
+
+	   std::streampos len = inFile.tellg();
+	   if (!inFile)
+	   {
+		  throw std::exception("SyncReadFile");
+	   }
+
+	   pBlob->resize(size_t(len));
+
+	   inFile.seekg(0, std::ios::beg);
+	   if (!inFile)
+	   {
+		  throw std::exception("SyncReadFile");
+	   }
+
+	   inFile.read(reinterpret_cast<char*>(pBlob->data()), len);
+	   if (!inFile)
+	   {
+		  throw std::exception("SyncReadFile");
+	   }
+
+	   inFile.close();
+
+	   return pBlob;
+	}
+}
 
 IApplication* const ApplicationTriangle::Factory(
 	const HWND hWnd, 
@@ -29,68 +70,70 @@ ApplicationTriangle::ApplicationTriangle(
 	)
    : IApplication(hWnd, bFullScreen, defaultWidth, defaultHeight)
 {
-   LOG_MESSAGE("ApplicationTriangle  ctor %p", this);
-   m_pDrawSystem = std::make_unique< DrawSystem>(hWnd);
+	LOG_MESSAGE("ApplicationTriangle  ctor %p", this);
+	m_pDrawSystem = std::make_unique< DrawSystem>(hWnd);
 
-   //std::vector< D3D12_INPUT_ELEMENT_DESC > inputElementDescArray;
-   //inputElementDescArray.push_back(D3D12_INPUT_ELEMENT_DESC{
-   //   "POSITION",
-   //   0,
-   //   DXGI_FORMAT_R32G32_FLOAT,
-   //   0,
-   //   D3D12_APPEND_ALIGNED_ELEMENT,
-   //   D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,
-   //   0 // UINT InstanceDataStepRate;
-   //   });
-   //inputElementDescArray.push_back(D3D12_INPUT_ELEMENT_DESC{
-   //   "COLOR",
-   //   0,
-   //   DXGI_FORMAT_R32G32B32A32_FLOAT,
-   //   0,
-   //   D3D12_APPEND_ALIGNED_ELEMENT,
-   //   D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,
-   //   0 // UINT InstanceDataStepRate;
-   //   });
+	std::vector< D3D12_INPUT_ELEMENT_DESC > inputElementDescArray({
+		D3D12_INPUT_ELEMENT_DESC{
+			"POSITION",
+			0,
+			DXGI_FORMAT_R32G32_FLOAT,
+			0,
+			D3D12_APPEND_ALIGNED_ELEMENT,
+			D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,
+			0 // UINT InstanceDataStepRate;
+			},
+		D3D12_INPUT_ELEMENT_DESC{
+			"COLOR",
+			0,
+			DXGI_FORMAT_R32G32B32A32_FLOAT,
+			0,
+			D3D12_APPEND_ALIGNED_ELEMENT,
+			D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,
+			0 // UINT InstanceDataStepRate;
+			}
+		});
 
-   //auto pCommandList = m_pDrawSystem->CreateCustomCommandList();
-   //{
-   //   auto pVertexShaderData = FileSystem::SyncReadFile(applicationParam.m_rootPath / "VertexShader.cso");
-   //   auto pPixelShaderData = FileSystem::SyncReadFile(applicationParam.m_rootPath / "PixelShader.cso");
-   //   std::vector<DXGI_FORMAT> renderTargetFormat;
-   //   renderTargetFormat.push_back(DXGI_FORMAT_B8G8R8A8_UNORM);
+	auto pCommandList = m_pDrawSystem->CreateCustomCommandList();
+	{
+		m_pGeometry = m_pDrawSystem->MakeGeometryGeneric(
+			pCommandList->GetCommandList(),
+			D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST,
+			inputElementDescArray,
+			std::vector<float>({
+				0.0f,0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 
+				0.5f,-0.5f, 0.0f, 1.0f, 0.0f, 1.0f,
+				-0.5f,-0.5f, 0.0f, 0.0f, 1.0f, 1.0f,
+				}),
+			6);
+	}
 
-   //   ShaderPipelineStateData shaderPipelineStateData(
-   //      inputElementDescArray,
-   //      D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE,
-   //      DXGI_FORMAT_UNKNOWN, // DXGI_FORMAT_D32_FLOAT,
-   //      renderTargetFormat,
-   //      CD3DX12_BLEND_DESC(D3D12_DEFAULT),
-   //      CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT),
-   //      CD3DX12_DEPTH_STENCIL_DESC() //CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT)
-   //      );
+	
+	{
+		auto pVertexShaderData = SyncReadFile("TriangleVertexShader.cso");
+		auto pPixelShaderData = SyncReadFile("TrianglePixelShader.cso");
+		std::vector<DXGI_FORMAT> renderTargetFormat;
+		renderTargetFormat.push_back(DXGI_FORMAT_B8G8R8A8_UNORM);
 
-   //   m_pShader = m_pDrawSystem->MakeShader(
-   //      pCommandList->GetCommandList(),
-   //      shaderPipelineStateData,
-   //      pVertexShaderData,
-   //      nullptr,
-   //      pPixelShaderData
-   //      );
-   //}
+		ShaderPipelineStateData shaderPipelineStateData(
+			inputElementDescArray,
+			D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE,
+			DXGI_FORMAT_UNKNOWN, // DXGI_FORMAT_D32_FLOAT,
+			renderTargetFormat,
+			CD3DX12_BLEND_DESC(D3D12_DEFAULT),
+			CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT),
+			CD3DX12_DEPTH_STENCIL_DESC() //CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT)
+			);
 
-   //{
-   //   std::vector< Vertex > vertexData({
-   //      {{0.0f,0.5f}, {1.0f, 0.0f, 0.0f, 1.0f}}, 
-   //      {{0.5f,-0.5}, {0.0f, 1.0f, 0.0f, 1.0f}},
-   //      {{-0.5f,-0.5}, {0.0f, 0.0f, 1.0f, 1.0f}},
-   //      });
-   //   m_pGeometry = m_pDrawSystem->MakeGeometry(
-   //      pCommandList->GetCommandList(),
-   //      D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST,
-   //      inputElementDescArray,
-   //      vertexData
-   //      );
-   //}
+		m_pShader = m_pDrawSystem->MakeShader(
+			pCommandList->GetCommandList(),
+			shaderPipelineStateData,
+			pVertexShaderData,
+			nullptr,
+			pPixelShaderData
+			);
+	}
+
 }
 
 ApplicationTriangle ::~ApplicationTriangle ()
@@ -99,8 +142,8 @@ ApplicationTriangle ::~ApplicationTriangle ()
    {
       m_pDrawSystem->WaitForGpu();
    }
-   //m_pShader.reset();
-   //m_pGeometry.reset();
+   m_pShader.reset();
+   m_pGeometry.reset();
    m_pDrawSystem.reset();
 
    LOG_MESSAGE("ApplicationTriangle  dtor %p", this);
@@ -113,8 +156,16 @@ void ApplicationTriangle ::Update()
 	{
 		auto pFrame = m_pDrawSystem->CreateNewFrame();
 		pFrame->SetRenderTarget(m_pDrawSystem->GetRenderTargetBackBuffer());
-	//   pFrame->SetShader(m_pShader.get());
-	//   pFrame->Draw(m_pGeometry.get());
+		auto pShader = m_pShader.get();
+		if (pShader)
+		{
+			pFrame->SetShader(pShader);
+		}
+		auto pGeometry = m_pGeometry.get();
+		if (pGeometry)
+		{
+			pFrame->Draw(pGeometry);
+		}
 	}
 }
 
