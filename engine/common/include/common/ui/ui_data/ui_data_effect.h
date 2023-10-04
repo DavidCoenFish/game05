527bc57a26285b90a589bdfcb0dfec6a5f5b397a
DavidCoenFish <fish_dsc@yahoo.co.uk>
2023-10-04 20:19:23 +1100

    2023-10-04T20:19:18 DESKTOP-LD41L9F

--- engine/common/include/common/ui/ui_data/ui_data_effect.h
+++ engine/common/include/common/ui/ui_data/ui_data_effect.h
@@ -4,19 +4,31 @@
 
 class Shader;
 
+enum class UIDataEffectType
+{
+    TNone,
+    TDropShadow,
+    TRoundCorners,
+    TBlendRGBAndAlpha,
+    TChromeFill
+};
+
 class UIDataEffect : public UIData
 {
 public:
     UIDataEffect(
-        const std::shared_ptr<Shader>& in_shader,
+        const UIDataEffectType in_type,
+        //const std::shared_ptr<Shader>& in_shader,
         const std::string& in_template_name = std::string("UIDataEffect"),
         const std::vector<std::shared_ptr<UIData>>& in_array_child_data = std::vector<std::shared_ptr<UIData>>()
         );
     virtual ~UIDataEffect();
 
-    const std::shared_ptr<Shader>& GetShaderRef() const { return _shader; }
+    //const std::shared_ptr<Shader>& GetShaderRef() const { return _shader; }
+    const UIDataEffectType GetEffectType() const { return _type; }
 
 private:
-    std::shared_ptr<Shader> _shader;
+    //std::shared_ptr<Shader> _shader;
+    UIDataEffectType _type;
 
 };
--- engine/common/source/common/ui/ui_data/ui_data_effect.cpp
+++ engine/common/source/common/ui/ui_data/ui_data_effect.cpp
@@ -2,7 +2,8 @@
 #include "common/ui/ui_data/ui_data_effect.h"
 
 UIDataEffect::UIDataEffect(
-    const std::shared_ptr<Shader>& in_shader,
+    //const std::shared_ptr<Shader>& in_shader,
+    const UIDataEffectType in_type,
     const std::string& in_template_name,
     const std::vector<std::shared_ptr<UIData>>& in_array_child_data
     )
@@ -10,7 +11,8 @@ UIDataEffect::UIDataEffect(
         in_template_name,
         in_array_child_data
         )
-    , _shader(in_shader)
+    //, _shader(in_shader)
+    , _type(in_type)
 {
     return;
 }
