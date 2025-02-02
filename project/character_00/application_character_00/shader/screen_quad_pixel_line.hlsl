1dc1dbd1ab1d5721015791322840046fd1d8cb46
DavidCoenFish <fish_dsc@yahoo.co.uk>
2025-02-02 19:14:59 +1100

    2025-02-02T19:14:54 DESKTOP-LD41L9F

--- project/character_00/application_character_00/shader/screen_quad_pixel_line.hlsl
+++ project/character_00/application_character_00/shader/screen_quad_pixel_line.hlsl
@@ -18,6 +18,8 @@ cbuffer ConstantBufferSphere : register(b1)
 };
 
 /*
+	https://stackoverflow.com/questions/2316490/the-algorithm-to-find-the-point-of-intersection-of-two-3d-line-segment/10288710#10288710
+
 	result[0] != 0.0 if result valid
 	result[1] distance along ray A of the nearest point between rays
 	result[2] distance along ray B of the nearest point between rays
@@ -52,64 +54,24 @@ Pixel main( Interpolant in_input )
 {
     Pixel result;
 
-	//https://stackoverflow.com/questions/2316490/the-algorithm-to-find-the-point-of-intersection-of-two-3d-line-segment/10288710#10288710
-	/*
-bool 
-intersection(const Line& a, const Line& b, Coord& ip)
-// http://mathworld.wolfram.com/Line-LineIntersection.html
-// in 3d; will also work in 2d if z components are 0
-{
-Coord da = a.second - a.first; 
-Coord db = b.second - b.first;
-Coord dc = b.first - a.first;
-
-if (dot(dc, cross(da,db)) != 0.0) // lines are not coplanar
-    return false;
-
-Point s = dot(cross(dc,db),cross(da,db)) / norm2(cross(da,db));
-if (s >= 0.0 && s <= 1.0)
-{
-    ip = a.first + da * Coord(s,s,s);
-    return true;
-}
-
-return false;
-}	
-    float3 world_eye_ray = g_texture0.Sample(g_sampler_state, in_input._uv).xyz;
-	float3 camera_pos = GetCameraPos();
-
-	float3 da = _ray_normal_length.xyz * _ray_normal_length.w;
-	float3 db = world_eye_ray;
-	float3 dc = camera_pos - _ray_origin_thickness.xyz;
-	float3 cross_da_db = cross(da, db);
-	float coplanar = dot(dc, cross_da_db);
-	if (coplanar == 0.0)
-	{
-		discard;
-	}
-	float s =  dot(cross(dc,db),cross(da,db)) / dot(cross_da_db, cross_da_db);
-	if ((s < 0.0) || (1.0 < s))
-	{
-		discard;
-	}
-	*/
-
 	float3 camera_pos = GetCameraPos();
     float3 camera_norm = g_texture0.Sample(g_sampler_state, in_input._uv).xyz;
 
 	float3 intersect = IntersectRayRay(camera_pos, camera_norm, _ray_origin_thickness.xyz, _ray_normal_length.xyz);
 
-	if ((intersect.x == 0.0) || 
-		(intersect.y < 0.0) || 
-		(intersect.z < 0.0) || 
-		(_ray_normal_length.w < intersect.z)
+	if ((intersect.x == 0.0) 
+		|| (intersect.y < 0.0) 
+		//|| (intersect.z < 0.0) 
+		//|| (_ray_normal_length.w < intersect.z)
 		)
 	{
 		discard;
 	}
 
+	float t = saturate(intersect.z / _ray_normal_length.w) * _ray_normal_length.w;
+
 	float3 camera_ray_nearest_point = camera_pos + (camera_norm * intersect.y);
-	float3 line_ray_nearest_point = _ray_origin_thickness.xyz + (_ray_normal_length.xyz * intersect.z);
+	float3 line_ray_nearest_point = _ray_origin_thickness.xyz + (_ray_normal_length.xyz * t);
 
 	float world_dist = distance(camera_ray_nearest_point, line_ray_nearest_point);
 
